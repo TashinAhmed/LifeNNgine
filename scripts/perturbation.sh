415fe2a7202fda61101192a9b58e16bed5d04656
Your Name <38316715+riveSunder@users.noreply.github.com>
2026-03-27 05:52:03 +0900

    modified perturbation script for experiment (temp)

--- scripts/perturbation.sh
+++ scripts/perturbation.sh
@@ -1,8 +1,8 @@
-python -m src.cann.perturb --filepath results/poly_l1_1/exp.csv --epoch_index -1 0 -e 67500 -r 10  --k_sign_perturbations 0 1 2 3 4 5 6 7 8 --uniform_magnitude 0.0 0.25 0.5 0.75 1.0 -x k_perturb_retrain_polykan_l1_1
+#python -m src.cann.perturb --filepath results/poly_l1_1/exp.csv --epoch_index -1 0 -e 67500 -r 10  --k_sign_perturbations 0 1 2 3 4 5 6 7 8 --uniform_magnitude 0.0 0.25 0.5 0.75 1.0 -x k_perturb_retrain_polykan_l1_1
+#
+python -m src.cann.perturb --filepath results/prelu_l1_1/exp.csv --epoch_index -1 0 -e 67500 -r 10  --k_sign_perturbations 0 1 2 3 4 5 6 7 8 --uniform_magnitude 0.0 0.25 0.5 0.75 1.0 -x k_perturb_retrain_prelu_l1_1
+#
+#python -m src.cann.perturb --filepath results/relu_l1_m/exp.csv  --epoch_index -1 0 -e 67500 -r 10  --k_sign_perturbations 0 1 2 3 4 5 6 7 8 --uniform_magnitude 0.0 0.25 0.5 0.75 1.0 -x k_perturb_retrain_relu_l1_m
 
-python -m src.cann.perturb --filepath results/prelu_l1_1/exp.csv b --epoch_index -1 0 -e 67500 -r 10  --k_sign_perturbations 0 1 2 3 4 5 6 7 8 --uniform_magnitude 0.0 0.25 0.5 0.75 1.0 -x k_perturb_retrain_prelu_l1_1
-
-python -m src.cann.perturb --filepath results/relu_l1_m/exp.csv  --epoch_index -1 0 -e 67500 -r 10  --k_sign_perturbations 0 1 2 3 4 5 6 7 8 --uniform_magnitude 0.0 0.25 0.5 0.75 1.0 -x k_perturb_retrain_relu_l1_m
-
-python -m src.cann.perturb --filepath results/silu_l1_1/exp.csv  --epoch_index -1 0 -e 67500 -r 10  --k_sign_perturbations 0 1 2 3 4 5 6 7 8 --uniform_magnitude 0.0 0.25 0.5 0.75 1.0 -x k_perturb_retrain_silu_l1_1
+#python -m src.cann.perturb --filepath results/silu_l1_1/exp.csv  --epoch_index -1 0 -e 67500 -r 10  --k_sign_perturbations 0 1 2 3 4 5 6 7 8 --uniform_magnitude 0.0 0.25 0.5 0.75 1.0 -x k_perturb_retrain_silu_l1_1
 
